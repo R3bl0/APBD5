d11d2e9a1c79c2f55b221a0d149cb13f4a022eea
R3bl0 <mlaskowski512@gmail.com>
2024-04-21 20:23:35 +0200

    base controller

--- Animals/Animals/Controllers/AnimalsController.cs
+++ Animals/Animals/Controllers/AnimalsController.cs
@@ -1,9 +1,18 @@
-﻿using System.Data.SqlClient;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace Animals.Controllers
 {
-    public class AnimalsController
+    [ApiController]
+    [Route("controller-animals")]
+    public class AnimalsController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public AnimalsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         
     }
 }
