4517b8e4df2b172c9fc0805e44d52d8ab6092c67
R3bl0 <mlaskowski512@gmail.com>
2024-04-22 19:16:12 +0200

    new connection string & httpget

--- Animals/Animals/Controllers/AnimalsController.cs
+++ Animals/Animals/Controllers/AnimalsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Animals.DTOs;
+using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
 
 namespace Animals.Controllers
@@ -13,6 +14,49 @@ namespace Animals.Controllers
         {
             _configuration = configuration;
         }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var response = new List<GetAnimalsResponse>();
+            using (var sqlConnection = new SqlConnection(_configuration.GetConnectionString("Default")))
+            {
+                var sqlCommand = new SqlCommand("SELECT * FROM animals", sqlConnection);
+                sqlCommand.Connection.Open();
+                var reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    response.Add(new GetAnimalsResponse(
+                        reader.GetInt32(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                        reader.GetString(4)
+                    ));
+                }
+            }
+
+            return Ok(response);
+        }
         
+        [HttpGet("{id}")]
+        public IActionResult GetAnimal(int id)
+        {
+            var response = new List<GetAnimalResponse>();
+            var sqlConnection = new SqlConnection(_configuration.GetConnectionString("Default"));
+            var sqlCommand = new SqlCommand("SELECT * FROM animals WHERE Id_Animal = @1", sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@1", id);
+            sqlCommand.Connection.Open();
+            var reader = sqlCommand.ExecuteReader();
+            if (!reader.Read()) return NotFound();
+            return Ok(new GetAnimalResponse(
+                        reader.GetInt32(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                        reader.GetString(4)
+                )
+            );
+        }
     }
 }
